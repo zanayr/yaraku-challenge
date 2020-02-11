ec619852df8edbe710be66796bb763d379c187dc
Ryan Fickenscher <rsficken@gmail.com>
2020-02-11 13:13:59 -0700

    Migration of  table, set up database api routes, created  model and added index, store and update methods to the  class.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index() {
+        return Book::all()->toJson();
+    }
+
+    public function store(Request $request) {
+        $book = Book::create($request->validate([
+            'title' => 'required',
+            'author' => 'required'
+        ]));
+        return response()->json('A book has been inserted into the database.');
+    }
+    
+    public function update($id, $data) {
+        $book = Book::findOrFail($id);
+        $book->title = $data->title;
+        $book->author = $data->author;
+        $book->update();
+        return response()->json('A book has been updated in the database.');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('books', 'BookController@index');
+Route::post('books', 'BookController@store');
+Route::put('books/{id}', 'BookController@update');
